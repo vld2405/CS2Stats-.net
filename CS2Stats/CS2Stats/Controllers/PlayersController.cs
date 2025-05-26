88fc897a5a53314d8f68f053fdf5d325d080fd91
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-05-26 12:59:04 +0300

    Added filters to the endpoint

--- CS2Stats/CS2Stats/Controllers/PlayersController.cs
+++ CS2Stats/CS2Stats/Controllers/PlayersController.cs
@@ -1,6 +1,7 @@
 ﻿using CS2Stats.Core.Dtos.Requests.Players;
 using CS2Stats.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 
 namespace CS2Stats.Api.Controllers
 {
@@ -19,15 +20,29 @@ namespace CS2Stats.Api.Controllers
 
         [HttpGet("get-players")]
         public async Task<IActionResult> GetPlayers(
-            [FromQuery] int page = 1,
-            [FromQuery] int pageSize = 5)
+            [FromQuery] int pageIndex = 1,
+            [FromQuery] int shownItems = 5,
+            [FromQuery] string? team = null,
+            [FromQuery] string? country = null)
         {
             var result = await playersService.GetPlayersAsync();
 
-            var pagedResult = result.Players
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .ToList(); 
+            var filteredPlayers = result.Players.AsQueryable();
+
+            if (team != null)
+            {
+                filteredPlayers = filteredPlayers.Where(p => p.TeamName.Equals(team, StringComparison.OrdinalIgnoreCase));
+            }
+            
+            if (country != null)
+            {
+                filteredPlayers = filteredPlayers.Where(p => p.Country.Equals(country, StringComparison.OrdinalIgnoreCase));
+            }
+
+            var pagedResult = filteredPlayers
+                .Skip((pageIndex - 1) * shownItems)
+                .Take(shownItems)
+                .ToList();
 
             return Ok(pagedResult);
         }
